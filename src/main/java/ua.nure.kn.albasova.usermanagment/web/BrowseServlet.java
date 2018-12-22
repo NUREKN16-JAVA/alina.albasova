5bed9366897a9c88637de3c5f3ea9dc7bc4bdcc1
AlinaAlbasova <albasovaalina@gmail.com>
2018-12-22 17:17:30 +0200

    22 step, find bug

--- src/main/java/ua.nure.kn.albasova.usermanagment/web/BrowseServlet.java
+++ src/main/java/ua.nure.kn.albasova.usermanagment/web/BrowseServlet.java
@@ -41,6 +41,7 @@ public class BrowseServlet extends HttpServlet {
     }
 
     private void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/add.jsp").forward(req, resp);
     }
 
     private void edit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
